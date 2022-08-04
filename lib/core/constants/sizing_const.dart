6bd8082a8b5a8de0f12c2b1281658871ecb89174
Dhiyaaul Auliyaa <ahmad.auliyaa@stockbit.com>
2022-08-04 09:48:56 +0700

    Constants | Add constants files
    a

---
+++ lib/core/constants/sizing_const.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SizingConst {
+  static double get defaultPadding => 18.sp;
+  static double get childPadding => 16.sp;
+  static double get defaultIconSize => 24.sp;
+}
