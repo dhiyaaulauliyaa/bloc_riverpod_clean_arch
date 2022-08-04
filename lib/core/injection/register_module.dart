d597e8a9aad4cbfb9f6532844ae15425e5f6a338
Dhiyaaul Auliyaa <ahmad.auliyaa@stockbit.com>
2022-08-04 10:02:50 +0700

    Network | Create network files

--- lib/core/injection/register_module.dart
+++ lib/core/injection/register_module.dart
@@ -1,9 +1,14 @@
 import 'package:data_connection_checker_tv/data_connection_checker.dart';
+import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
+
+import '../network/network_client.dart';
 
 @module
 abstract class RegisterModule {
   /* ------------------ Network Module ------------------ */
+  @lazySingleton
+  Dio get dio => NetworkClient.init();
   @lazySingleton
   DataConnectionChecker get dataConnectionChecker => DataConnectionChecker();
 }
---
+++ lib/core/network/network_module.dart
@@ -0,0 +1,170 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+
+import '../errors/exceptions.dart';
+import '../injection/service_locator.dart';
+import '../utils/log.dart';
+
+abstract class NetworkModule {
+  final _dio = getIt<Dio>();
+
+  /* Headers */
+  Future<Map<String, String>> headers() async {
+    return {
+      'Content-Type': 'application/json',
+    };
+  }
+
+  Future<Map<String, dynamic>> safeCallApi<T>(
+    Future<Response<T>> call,
+  ) async {
+    try {
+      var response = await call;
+      var data = responseParser(response);
+
+      if (data['status'] == 'success') return data;
+
+      if (data['response'] != null) {
+        if (data['request'] == data['success']) return data;
+
+        var firstError = (data['response'] as List).firstWhere(
+          (d) => d['status'] != 'success',
+        );
+        data['status'] = firstError['status'];
+        data['message'] = firstError['message'];
+      }
+
+      throw ServerException(code: data['status'], message: data['message']);
+    }
+
+    /* On Known Error */
+    on DioError catch (dioErr) {
+      String message = dioErr.message;
+      String? code = '500';
+
+      Log.error('Dio Error [${dioErr.type}]', type: 'Err');
+      Log.soft(message, type: 'Err');
+
+      List<DioErrorType> dioTimeout = [
+        DioErrorType.connectTimeout,
+        DioErrorType.receiveTimeout,
+        DioErrorType.sendTimeout,
+      ];
+
+      if (dioTimeout.any((e) => dioErr.type == e)) {
+        throw NetworkException();
+      }
+
+      if (dioErr.type == DioErrorType.response) {
+        try {
+          message = dioErr.response?.data['message'];
+          code = dioErr.response?.statusCode?.toString();
+        } catch (parseErr) {
+          Log.warning('Parse Error', type: 'Err');
+          Log.soft(
+            'Unknown error message | ${dioErr.response?.realUri}',
+            type: 'ERR',
+          );
+          Log.divider();
+
+          message = 'N/A';
+        }
+      }
+
+      throw ServerException(message: message, code: code);
+    }
+  }
+
+  Map<String, dynamic> responseParser(Response response) {
+    var data = <String, dynamic>{};
+
+    try {
+      data = json.decode(response.toString());
+    } catch (e) {
+      Log.warning('Parse Error', type: 'RESP');
+      Log.soft(
+        'Response format isn\'t as expected | ${response.realUri}',
+        type: 'RESP',
+      );
+      Log.divider();
+    }
+
+    return data;
+  }
+
+  Future<Map<String, dynamic>> getMethod(
+    String endpoint, {
+    Map<String, dynamic>? params,
+  }) async {
+    Options options = Options(
+      headers: await headers(),
+    );
+
+    var response = await safeCallApi(
+      _dio.get(
+        endpoint,
+        queryParameters: params,
+        options: options,
+      ),
+    );
+
+    return response;
+  }
+
+  Future<Map<String, dynamic>> postMethod(
+    String endpoint, {
+    Map<String, dynamic>? body,
+  }) async {
+    Options options = Options(headers: await headers());
+
+    var response = await safeCallApi(
+      _dio.post(
+        endpoint,
+        data: body,
+        options: options,
+      ),
+    );
+
+    return response;
+  }
+
+  Future<Map<String, dynamic>> putMethod(
+    String endpoint, {
+    Map<String, dynamic>? body,
+  }) async {
+    Options options = Options(headers: await headers());
+
+    var response = await safeCallApi(
+      _dio.put(
+        endpoint,
+        data: body,
+        options: options,
+      ),
+    );
+
+    return response;
+  }
+
+  Future<Map<String, dynamic>> deleteMethod(
+    String endpoint, {
+    Map<String, dynamic>? body,
+  }) async {
+    Options options = Options(headers: await headers());
+
+    var response = await safeCallApi(
+      _dio.delete(
+        endpoint,
+        data: body,
+        options: options,
+      ),
+    );
+
+    return response;
+  }
+}
+
+@LazySingleton(as: NetworkModule)
+class NetworkModuleImpl extends NetworkModule {}
