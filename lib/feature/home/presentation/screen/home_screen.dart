90d449fcb6f2439726ce8d9a49d8127289b3d6a5
Dhiyaaul Auliyaa <ahmad.auliyaa@stockbit.com>
2022-08-05 10:00:26 +0700

    Home | Create HomeScreen
    a

---
+++ lib/feature/home/presentation/screen/home_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildNavButton(
+              context,
+              title: 'Dummy Data',
+              nextPage: const SizedBox(),
+            ),
+            _buildNavButton(
+              context,
+              title: 'BLoC (Cubit)',
+              nextPage: const SizedBox(),
+            ),
+            _buildNavButton(
+              context,
+              title: 'Riverpod',
+              nextPage: const SizedBox(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavButton(
+    BuildContext context, {
+    required String title,
+    required Widget nextPage,
+  }) {
+    return SizedBox(
+      width: 1.sw * 0.4,
+      child: ElevatedButton(
+        onPressed: () => Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (_) => nextPage,
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          primary: Colors.tealAccent,
+          onPrimary: Colors.black,
+        ),
+        child: Text(
+          title,
+          style: TextStyle(
+            fontSize: 16.sp,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+}
