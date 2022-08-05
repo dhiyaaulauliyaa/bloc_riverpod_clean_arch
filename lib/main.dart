00732ae05d790f757da1002156401af28999b992
Dhiyaaul Auliyaa <ahmad.auliyaa@stockbit.com>
2022-08-05 20:38:38 +0700

    Main | Add ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -15,15 +16,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(375, 812),
-      builder: (_, Widget? __) => MaterialApp(
-        title: 'MotionApp',
-        home: const HomeScreen(),
-        theme: ThemeData(
-          brightness: Brightness.dark,
-          textTheme: GoogleFonts.spaceGroteskTextTheme().apply(
-            bodyColor: Colors.white,
+    return ProviderScope(
+      child: ScreenUtilInit(
+        designSize: const Size(375, 812),
+        builder: (_, Widget? __) => MaterialApp(
+          title: 'MotionApp',
+          home: const HomeScreen(),
+          theme: ThemeData(
+            brightness: Brightness.dark,
+            textTheme: GoogleFonts.spaceGroteskTextTheme().apply(
+              bodyColor: Colors.white,
+            ),
           ),
         ),
       ),
