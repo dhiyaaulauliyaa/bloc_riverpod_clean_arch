e168a0c6ce240677a7d354758f7aa7a2c99f52bc
Dhiyaaul Auliyaa <ahmad.auliyaa@stockbit.com>
2022-08-05 11:12:15 +0700

    Product | Implement RemoteDataSource

--- lib/feature/product/data/repositories/product_repository_impl.dart
+++ lib/feature/product/data/repositories/product_repository_impl.dart
@@ -3,20 +3,39 @@ import 'package:injectable/injectable.dart';
 
 import '../../../../core/base/base_usecase.dart';
 import '../../../../core/base/repository_mixin.dart';
+import '../../../../core/errors/exceptions.dart';
 import '../../../../core/errors/failures.dart';
 import '../../../../core/injection/service_locator.dart';
+import '../../../../core/utils/log.dart';
 import '../../domain/entities/product.dart';
 import '../../domain/repositories/product_repository.dart';
 import '../datasources/product_dummy_datasource.dart';
+import '../datasources/product_remote_datasource.dart';
 
 @LazySingleton(as: ProductRepository)
 class ProductRepositoryImpl with RepositoryMixin implements ProductRepository {
+  final remoteDataSource = getIt<ProductRemoteDataSource>();
   final dummyDataSource = getIt<ProductDummyDataSource>();
 
   @override
-  Future<Either<Failure, List<Product>>> getProducts(NoParams params) {
-    return dummyDataSource.getProducts.call().then(
-          (products) => Right(products),
-        );
+  Future<Either<Failure, List<Product>>> getProducts(NoParams params) async {
+    // return dummyDataSource.getProducts.call().then(
+    //       (products) => Right(products),
+    //     );
+
+    try {
+      var result = await remoteDataSource.getProducts();
+
+      return Right(result);
+    } on ServerException catch (e) {
+      return Left(ServerFailure(message: e.message, code: e.code));
+    } on NetworkException catch (e) {
+      return Left(NetworkFailure(message: e.message));
+    } catch (e) {
+      /* Do some error logging */
+      Log.error('Unhandled error: $e');
+
+      return const Left(ClientFailure(message: 'Something went wrong'));
+    }
   }
 }
