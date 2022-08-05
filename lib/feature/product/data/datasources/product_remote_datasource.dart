dc2cced2bd4cfcc7ea7ff68f701076fc165c919a
Dhiyaaul Auliyaa <ahmad.auliyaa@stockbit.com>
2022-08-05 11:12:15 +0700

    Product | Implement RemoteDataSource

---
+++ lib/feature/product/data/datasources/product_remote_datasource.dart
@@ -0,0 +1,25 @@
+import 'package:injectable/injectable.dart';
+
+import '../../../../core/injection/service_locator.dart';
+import '../../../../core/network/network_module.dart';
+import '../../../../core/network/network_url.dart';
+import '../../domain/entities/product.dart';
+import '../models/product_model.dart';
+
+abstract class ProductRemoteDataSource {
+  Future<List<Product>> getProducts();
+}
+
+@LazySingleton(as: ProductRemoteDataSource)
+class ProductRemoteDataSourceImpl implements ProductRemoteDataSource {
+  final networkModule = getIt<NetworkModule>();
+
+  @override
+  Future<List<Product>> getProducts() async {
+    var result = await networkModule.getMethod(
+      NetworkURL.products,
+    );
+
+    return ProductModel.fromJsonList(result['products']);
+  }
+}
