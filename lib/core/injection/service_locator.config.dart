11ac8980f6117f1c3a56180be24ae482ce7fe91b
Dhiyaaul Auliyaa <ahmad.auliyaa@stockbit.com>
2022-08-05 11:19:28 +0700

    Update service locator

--- lib/core/injection/service_locator.config.dart
+++ lib/core/injection/service_locator.config.dart
@@ -10,16 +10,20 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
 import '../../feature/product/data/datasources/product_dummy_datasource.dart'
-    as _i9;
-import '../../feature/product/data/repositories/product_repository_impl.dart'
-    as _i11;
-import '../../feature/product/domain/repositories/product_repository.dart'
     as _i10;
+import '../../feature/product/data/datasources/product_remote_datasource.dart'
+    as _i11;
+import '../../feature/product/data/repositories/product_repository_impl.dart'
+    as _i13;
+import '../../feature/product/domain/repositories/product_repository.dart'
+    as _i12;
 import '../../feature/product/domain/usecases/get_products.dart' as _i5;
+import '../../feature/product/presentation/cubits/product/product_cubit.dart'
+    as _i9;
 import '../network/network_info.dart' as _i7;
 import '../network/network_module.dart' as _i8;
 import '../services/messenger.dart' as _i6;
-import 'register_module.dart' as _i12; // ignore_for_file: unnecessary_lambdas
+import 'register_module.dart' as _i14; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -34,10 +38,13 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
   gh.lazySingleton<_i6.MessengerService>(() => _i6.MessengerService());
   gh.lazySingleton<_i7.NetworkInfo>(() => _i7.NetworkInfoImpl());
   gh.lazySingleton<_i8.NetworkModule>(() => _i8.NetworkModuleImpl());
-  gh.lazySingleton<_i9.ProductDummyDataSource>(
-      () => _i9.ProductDummyDataSourceImpl());
-  gh.lazySingleton<_i10.ProductRepository>(() => _i11.ProductRepositoryImpl());
+  gh.factory<_i9.ProductCubit>(() => _i9.ProductCubit());
+  gh.lazySingleton<_i10.ProductDummyDataSource>(
+      () => _i10.ProductDummyDataSourceImpl());
+  gh.lazySingleton<_i11.ProductRemoteDataSource>(
+      () => _i11.ProductRemoteDataSourceImpl());
+  gh.lazySingleton<_i12.ProductRepository>(() => _i13.ProductRepositoryImpl());
   return get;
 }
 
-class _$RegisterModule extends _i12.RegisterModule {}
+class _$RegisterModule extends _i14.RegisterModule {}
